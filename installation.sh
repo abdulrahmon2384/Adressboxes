5fb23af68d93167a995e8cbea2e55ed728ea22f5
Abdul rahmon <abdulrahmon2384@gmail.com>
2023-02-22 09:42:08 +0000

    Delete installation.sh

--- installation.sh
+++
@@ -1,7 +0,0 @@
-echo "# abdulrahmon2384" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/abdulrahmon2384/abdulrahmon2384.git
-git push -u origin main
